197fe44230e59e885de209539ed4c47870e5c025
Saif Ali <saifsolangi2@gmail.com>
2023-08-30 13:02:32 +0500

    data fetching

--- app/api/search-address/route.tsx
+++ app/api/search-address/route.tsx
@@ -1,13 +1,24 @@
-import { request } from "http";
 import { NextResponse } from "next/server";
-
-const BASE_URL = 'https://api.mapbox.com/search/searchbox/v1/suggest'
-
+const BASE_URL = "https://api.mapbox.com/search/searchbox/v1/suggest"
 export async function GET(request: any) {
 
-    const { searchParams } = new URL(request.url)
-    const searchText = searchParams.get('q')
+    const { searchParams } = new URL(request.url);
 
-    const res = await fetch(BASE_URL + '?q=' + searchText + '?language=en&limit=1&session_token=[GENERATED-UUID]&proximity=-83.748708,42.265837&country=US')
-    return NextResponse.json({ data: searchText })
+    const searchText = searchParams.get('q');
+
+    const apiUrl = BASE_URL + '?q=' + searchText + '?language=en&limit=6&session_token=5ccce4a4-ab0a-4a7c-943d-580e55542363'
+        + "&access_token=" + process.env.MAPBOX_ACCESS_TOKEN
+
+    const res = await fetch(apiUrl, {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    console.log("Response Headers:", res.headers);
+
+    const searchResult = await res.json();
+    console.log("Response Content:", searchResult);
+
+    return NextResponse.json(searchResult);
 }
